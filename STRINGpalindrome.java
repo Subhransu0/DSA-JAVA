4bdcf1704013f283862fc02ddf3b677e5d42f151
SUBHRANSU MOHAN DAS <dassubhransu9451@gmail.com>
2024-03-08 18:55:03 +0530

    String is Palindrome or not.
    To find out whether the given String is Palindrome or not.

--- STRINGpalindrome.java
+++ STRINGpalindrome.java
@@ -1,21 +1,23 @@
+
 // To find out whether the given String is Palindrome or not.
 import java.util.*;
+
 public class STRINGpalindrome {
-public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    String num = in.nextLine();
-    int i = 0 ;
-    int j = num.length()-1;
-    while (i<j) {
-        if (num.charAt(i)!=num.charAt(j)) {
-            System.out.println("Not a palindrome .");
-            System.exit(0);
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String num = in.nextLine();
+        int i = 0;
+        int j = num.length() - 1;
+        while (i < j) {
+            if (num.charAt(i) != num.charAt(j)) {
+                System.out.println("Not a palindrome .");
+                System.exit(0);
+            }
+            i++;
+            j--;
         }
-        i++;
-        j--;
+        System.out.println("Palindrome .");
+        in.close();
     }
-    System.out.println("Palindrome .");
-    
-}
-    
+
 }
