bc406ab71d7520cde3e34b48ed9c0483e613e5d9
SUBHRANSU MOHAN DAS <dassubhransu9451@gmail.com>
2024-03-08 19:42:47 +0530

    ODD or EVEN using METHOD/FUNCTION
    Define a program to find out whether a given number is even or odd.

--- ODDorEVEN.java
+++ ODDorEVEN.java
@@ -1,20 +1,25 @@
 
-// Write a program to print whether a number is even or odd, also take input from the user (using While loop)
+// Define a program to find out whether a given number is even or odd.
 import java.util.Scanner;
 
 public class ODDorEVEN {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        while (true) {
-            System.out.println("Enter a number to check odd or even : ");
-            int num = in.nextInt();
-            if (num % 2 == 0) {
-                System.out.println("The number is even");
-            } else {
-                System.out.println("The number is odd");
-            }
-            in.close();
+        System.out.println("Enter a number to check odd or even : ");
+        int num = in.nextInt();
+        if (EVEN(num)) {
+            System.out.println("Even number .");
+        } else {
+            System.out.println("Odd number .");
         }
+        in.close();
+    }
+
+    static boolean EVEN(int num) {
+        if (num % 2 == 0) {
+            return true;
+        }
+        return false;
     }
 
 }
