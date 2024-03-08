0b2d5305eecad1feab1299820fbb3b6c73d5248c
SUBHRANSU MOHAN DAS <dassubhransu9451@gmail.com>
2024-03-08 18:32:46 +0530

    multiplication table
    Take a number as input and print the multiplication table for it.

--- Multiplicationtable.java
+++ Multiplicationtable.java
@@ -1,7 +1,6 @@
 
 // Take a number as input and print the multiplication table for it.
 import java.util.*;
-
 public class Multiplicationtable {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
