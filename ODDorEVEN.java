ba39d59ebca25cbb1f76415c8d630f975fc31a50
SUBHRANSU MOHAN DAS <dassubhransu9451@gmail.com>
2024-03-08 18:34:14 +0530

    ODD or EVEN

--- ODDorEVEN.java
+++ ODDorEVEN.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class ODDorEVEN {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
         while (true) {
             System.out.println("Enter a number to check odd or even : ");
             int num = in.nextInt();
