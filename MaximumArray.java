1566cd079c5a0763890940a783d86411d1daf394
SUBHRANSU MOHAN DAS <dassubhransu9451@gmail.com>
2024-03-10 11:10:29 +0530

    Find Maximum Value in array .
    Find Maximum Value in array .

---
+++ MaximumArray.java
@@ -0,0 +1,17 @@
+//Find Maximum Value in array .
+public class MaximumArray {
+  public static void main(String[] args) {
+    int [] arr ={55,66,70,90,36,21,55};
+    System.out.println("Maximum value is : "+MAX(arr));
+
+  } 
+  static int MAX(int [] arr){
+    int Minmum = Integer.MIN_VALUE;
+    for (int i = 0; i < arr.length; i++) {
+        if (Minmum < arr[i]) {
+            Minmum = arr[i];
+        }
+    }
+    return Minmum;
+  } 
+}
