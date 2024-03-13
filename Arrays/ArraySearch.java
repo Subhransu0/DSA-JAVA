ca9489015923bf0a0ea293652810ec65ee780335
SUBHRANSU MOHAN DAS <dassubhransu9451@gmail.com>
2024-03-13 18:42:04 +0530

    Arrays questions

---
+++ Arrays/ArraySearch.java
@@ -0,0 +1,23 @@
+// WAP to perferm SEARCH Opeartion in java .
+public class ArraySearch {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+        int target = 7;
+        int result = SEARCH(arr, target);
+        System.out.println(result);
+    }
+
+    static int SEARCH(int[] arr, int target) {
+        if (arr.length == 0) {
+            return -1;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            int element = arr[i];
+            if (element == target) {
+                return i;
+            }
+        }
+        return 1;
+    }
+
+}
---
+++ Arrays/MULTIarray.java
@@ -0,0 +1,32 @@
+//WAP To perferm search operation IN Multidimensional array .2D.
+
+import java.util.Arrays;
+
+public class MULTIarray {
+
+    public static void main(String[] args) {
+        int[][] arr = {
+                { 12, 13, 14, 15 },
+                { 16, 17, 18, 19, 20 },
+                { 21, 22, 23, 34, 45, 56, 76 },
+                { 1, 3, 8, 9 },
+                { 99 }
+        };
+
+        int target = 99;
+        System.out.println(Arrays.toString(SEARCH(arr, target)));
+
+    }
+
+    static int[] SEARCH(int[][] arr, int target) {
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                int element = arr[row][col];
+                if (element == target) {
+                    return new int[] { row, col };
+                }
+            }
+        }
+        return new int[] { -1, -1 };
+    }
+}
