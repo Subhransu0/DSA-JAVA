3754062eed9a6f01075fd20fa73a66e6f5dea9cc
SUBHRANSU MOHAN DAS <dassubhransu9451@gmail.com>
2024-03-14 13:46:56 +0530

    BINARY SEARCH

--- Arrays/BInarySearch.java
+++ Arrays/BInarySearch.java
@@ -1,39 +1,45 @@
-// WAP to perferm search operation in ascending or descendin order 
-public class BInarySearch {
+import java.util.*;
+
+
+public class BINARYSEARCH{
 
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }; // sorted array in ascending order
-        int [] arr2 = {100,55,44,33,22,20,19,18,17,15,10,5};
-        int target = 14;
-        int target2 = 15;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter array element : ");
+        int[] arr = new int[5];
 
-        System.out.println(SERACH(arr, target));
-        System.err.println(SERACH(arr2, target2));
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Enter target : ");
+        int target = in.nextInt();
 
+        System.out.println(BINARYSEARCH(arr, target));
     }
 
-    static int SERACH(int[] arr, int target) {
-
+    static int BINARYSEARCH(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
         boolean Isasc = arr[start] < arr[end];
-        while (start <= end) {
+        while (start < end) {
             int mid = start + (end - start) / 2;
             if (arr[mid] == target) {
                 return mid;
             }
             if (Isasc) {
-                if (target < arr[mid]) {
+                if (arr[mid] > target) {
                     end = mid - 1;
                 } else {
                     start = mid + 1;
                 }
             } else {
-                if (target > arr[mid]) {
-                     end = mid - 1;
-                } else {
+                if (arr[mid] > target) {
                     start = mid + 1;
+                } else {
+                    end = mid - 1;
                 }
+
             }
         }
         return -1;
